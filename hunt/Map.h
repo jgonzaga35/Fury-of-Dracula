91967c67b5f8d941dbfbefd16ef234602903d6ea
z5312819 <prayag16@outlook.com>
2020-07-29 22:50:45 +1000

    added several strats for hunters

--- hunt/Map.h
+++ hunt/Map.h
@@ -14,6 +14,8 @@
 #include <stdlib.h>
 
 #include "Places.h"
+#include "Game.h"
+#include "Queue.h"
 
 #ifndef FOD__MAP_H_
 #define FOD__MAP_H_
@@ -48,5 +50,28 @@ int MapNumConnections(Map m, TransportType type);
  *  The returned list should NOT be modified or freed.
  */
 ConnList MapGetConnections(Map m, PlaceId p);
+
+
+///////////////////////////////////////////////////////////////////////////////////
+// prints all nodes in connection list
+void printConnList(ConnList L);
+
+// returns path array from "from" city to all other cities
+// through edges of "type"
+// Player argument to satisfy conditions that Dracula
+// cannot move to the hospital
+// If no condition needed for argument Player enter -1
+void bfsPath(Map m, PlaceId *visited, PlaceId from, bool road, bool rail, bool boat, Player p);
+
+/** From list of connections (provided by MapGetconnections function),
+ * scan through linked list, simultaneously adding
+ * "type" connection to allowableCNC array.
+ * Update number of unique locations added to array through numReturnedLocs */
+void getRoadCNC(ConnList CNC, PlaceId *allowableCNC, int *numReturnedLocs, Player p);
+void getRailCNC(ConnList CNC, PlaceId from, PlaceId *allowableCNC, int *numReturnedLocs, Round round, Player player, Map m);
+void getBoatCNC(ConnList CNC, PlaceId *allowableCNC, int *numReturnedLocs, Player p);
+
+// returns array of neighbouring PlaceIds
+PlaceId *getConnection(Map map, PlaceId src, int *numReturnedLocs);
 
 #endif // !defined(FOD__MAP_H_)
--- hunt/hunter.c
+++ hunt/hunter.c
@@ -9,6 +9,7 @@
 //
 ////////////////////////////////////////////////////////////////////////
 
+#include <stdio.h>
 #include "Game.h"
 #include "hunter.h"
 #include "HunterView.h"
@@ -26,7 +27,7 @@ void decideHunterMove(HunterView hv)
 		// I chose corners of the map - not sure if best choice
 		// (need at least one hunter in middle)
 		// Need to discuss this
-		switch (name) {
+		switch(name) {
 			case PLAYER_LORD_GODALMING:
 				location = "ED";
 				break;
@@ -44,23 +45,71 @@ void decideHunterMove(HunterView hv)
 		}
 		
 		registerBestPlay(location, "");
-	} else { // for all other rounds
+		return;
+	} 
+	// for all other rounds
+		PlaceId HunterLoc, DraculaLoc, VampireLoc;
+		Round round = HvGetRound(hv);
+		Player currHunter = HvGetPlayer(hv);
+		char *play; // move to make -- this is sent to register best play
+		Mesage message; // message -- this is sent to register best play
 
-		// Add ideas for strats before making function
+		// ---------------Get current location of the hunter--------------------
+		switch(currHunter) {
+			case PLAYER_LORD_GODALMING:
+				HunterLoc = HvGetPlayerLocation(hv, currHunter);
+				break;
+			case PLAYER_DR_SEWARD:
+				HunterLoc = HvGetPlayerLocation(hv, currHunter);
+				break;
+			case PLAYER_VAN_HELSING:
+				HunterLoc = HvGetPlayerLocation(hv, currHunter);
+				break;
+			case PLAYER_MINA_HARKER:
+				HunterLoc = HvGetPlayerLocation(hv, currHunter);
+				break;
+			default:
+				break;
+		}
 
-		/** Strats:
-		 * 	- 	Use HvWhereCanTheyGoByType (remove rail) function to decide 
-		 * 		where exactly Dracula is able to travel. use this info to plan
-		 * 		where hunter should move (possibly in 5 city radii around that
-		 * 		location)
-		 * 	- 	Whenever (round + num) % 4 ==3, decide if hunters can intercept 
-		 * 		Dracula by moving via rail (Dracula's location decided by 
-		 * 		HvGetLastKnownDraculaLocation)
-		 * */
-	}
+		// --------------Get last known Dracula location------------------------
+		/** Gets  Dracula's  last  known  real  location  as revealed in the 
+		  * play string and sets *round to the number of the  latest  round  in  
+		  * which Dracula moved there.*/
+		Round LastDracRoundSeen = -1;
+		DraculaLoc = HvGetLastKnownDraculaLocation(hv, &LastDracRoundSeen);
 
+		if(DraculaLoc != NOWHERE && LastDracRoundSeen != -1) { 
+			// Dracula's last real location is known
+			
+			// Depending on how far away the hunter is from Dracula,
+			// take different cases.
+			int pathLength = -1;
+			PlaceId *path = HvGetShortestPathTo(hv, currHunter, DraculaLoc, &pathLength);
+			// NOTE:: call to above function is expensive
+			 
 
+		} else {
+			// If Dracula's location not know, perform collab research
+			// This allows us to know the 6th move in Dracula's trail immediately
+			// Note:: If the move was a HIDE/DOUBLE_BACK move, then the move that
+			// the HIDE/DOUBLE_BACK refers to will be revealed (and so on
+			// until LOCATIO is revealed)
+			// Therefore, it might not exactly be the 6th last move
+			registerBestPlay(HunterLoc, "Research"); // sends currLocofHunter back
+		}
 
-	// TODO: Replace this with something better!
-	registerBestPlay("TO", "Have we nothing Toulouse?");
+		// ---------------------If Dracula health <= x--------------------------
+		if(HvGetHealth(hv, PLAYER_DRACULA) <= 15) {
+			// If Dracula's health is less than x, move towards Castle Dracula
+			// HOWEVER, if he is really far away, then try and kill him
+		}
+
+		// --------------If hunter health low, rest-----------------------------
+		int currHunterHealth = HvGetHealth(hv, currHunter);
+		if(currHunterHealth <= 3) {
+			registerBestPlay(HunterLoc, "Resting");
+		}
+
+	registerBestPlay(play, mesasge);
 }
