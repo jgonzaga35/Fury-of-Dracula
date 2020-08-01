4bfb074df2824b6e5f232c81c1108daca45e8659
z5255845 <justingonzaga35@gmail.com>
2020-08-01 17:45:16 +1000

    implementing dracula strat

--- hunt/dracula.c
+++ hunt/dracula.c
@@ -13,7 +13,6 @@
 #include "DraculaView.h"
 #include "Game.h"
 #include "Map.h"
-#include "Map.c"
 #include "Places.h"
 #include "Queue.h"
 #include <stdio.h>
@@ -23,21 +22,19 @@
 void removeRiskyLocs(PlaceId *validLocs, PlaceId *riskyLocs, int *numValidLocs, int *numRiskyLocs);
 void decideDraculaMove(DraculaView dv)
 {
-	Round round = DvGetRound(dv);
+	
+	Round round = DvGetRound(dv);	// The current round in the game.
+	char *play;						// The play to be made.
+
 	// First Round
-	// Dracula should start in a location adjacent to many other
-	// locations (by road), to avoid being cornered.
-	// Strasbourg has 8 adjacent road cities, so choose Strasbourg.
 	if (round == 0) {
 		registerBestPlay("ST", "Mwahahahaha");
 		return;
 	}
 
-	
 	// Get possible locations for Dracula. Return TELEPORT if no valid moves.
 	int numValidLocs = 0;
 	PlaceId *validLocs = DvWhereCanIGo(dv, &numValidLocs);
-
 	if (numValidLocs == 0) {
 		printf("no valid locs\n");
 		free(validLocs);
@@ -45,24 +42,31 @@ void decideDraculaMove(DraculaView dv)
 		return;
 	}
 
-	// Get reachable locations ("risky locations" for each hunter and remove these, if they exist, from Dracula's
+	// Go to Castle Dracula if possible - Dracula wants to gain 10 BP.
+	// Even if a hunter is there, it will be an even exchange. 
+	for (int i = 0; i < numValidLocs; i++) {
+		if (validLocs[i] == CASTLE_DRACULA) {
+			registerBestPlay("CD", "COMP2521 > COMP1511");
+			return;
+		} 
+	}
+
+	// Get reachable locations by road ("risky locations" for each hunter and remove these from Dracula's
 	// valid locations array (he wants to avoid these locations as much as possible).
 	int numRiskyLocs = 0;
 	for (int player = PLAYER_LORD_GODALMING; player < PLAYER_MINA_HARKER; player++) {
 		// riskyLocs should never be null as hunters always have a valid move.
-		PlaceId *riskyLocs = DvWhereCanTheyGo(dv, player, &numRiskyLocs);
+		PlaceId *riskyLocs = DvWhereCanTheyGoByType(dv, player, true, false, true, &numRiskyLocs);
 		removeRiskyLocs(validLocs, riskyLocs, &numValidLocs, &numRiskyLocs);
 		free(riskyLocs);
 	}
-	
-	// If all of Dracula's valid locations are also "risky" locations
+
+	// If all of Dracula's valid locations are also "risky" locations:
 	if (numValidLocs == 0) {
-		
 		// If possible go to the SEA to avoid encountering a hunter.
 		// (Its better to lose 2 BP at SEA than lose 10 BP to a hunter!)
 		validLocs = DvWhereCanIGoByType(dv, false, true, &numValidLocs);
 		if (validLocs != NULL) {
-			char *play;
 			int index = rand() % (numValidLocs + 1);
 			strcpy(play, placeIdToAbbrev(validLocs[index]));
 			free(validLocs);
@@ -72,23 +76,23 @@ void decideDraculaMove(DraculaView dv)
 		
 		// Otherwise choose random location in the original validLocs (not necessarily a good move!)
 		validLocs = DvWhereCanIGo(dv, &numValidLocs);
-		int index = rand() % (numValidLocs + 1);
-		char *play;
-		strcpy(play, placeIdToAbbrev(validLocs[index]));
-		free(validLocs);
-		registerBestPlay(play, "Mwahahahaha");
-		return;
+		numRiskyLocs = 0;
+		for (int player = PLAYER_LORD_GODALMING; player < PLAYER_MINA_HARKER; player++) {
+			// riskyLocs should never be null as hunters always have a valid move.
+			PlaceId *riskyLocs = DvWhereCanTheyGoByType(dv, player, false, true, false, &numRiskyLocs);
+			removeRiskyLocs(validLocs, riskyLocs, &numValidLocs, &numRiskyLocs);
+			free(riskyLocs);
+		}
 	}
+
 
 	// If Dracula can go to a location that is not "risky":
 	// Go to random location in validLocs (not necessarily a good move!)
-	char *play;
 	int index = rand() % (numValidLocs + 1);
 	strcpy(play, placeIdToAbbrev(validLocs[index]));
 	free(validLocs);
 	registerBestPlay(play, "Mwahahahaha");
 	return;
-
 }
 
 void removeRiskyLocs(PlaceId *validLocs, PlaceId *riskyLocs, int *numValidLocs, int *numRiskyLocs) {
